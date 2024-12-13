d074d5c13aba2db07622a7e58b0e2182d3ce0c80
Bogdan Bacoșcă <bogdanbacosca@protonmail.com>
2024-12-13 19:12:11 +0200

    new remember me feature

--- logout.php
+++ logout.php
@@ -1,5 +1,23 @@
 <?php
 session_start();
+include 'db.php';
+
+// Remove the remember me token from the database and cookie
+if (isset($_COOKIE['remember_token'])) {
+    $remember_token = $_COOKIE['remember_token'];
+
+    // Delete the token from the database
+    $token_sql = "DELETE FROM remember_tokens WHERE token = ?";
+    $stmt = $conn->prepare($token_sql);
+    $stmt->bind_param("s", $remember_token);
+    $stmt->execute();
+    $stmt->close();
+
+    // Delete the cookie
+    setcookie("remember_token", "", time() - 3600, "/", "", false, true);
+}
+
+// Destroy the session
 session_destroy();
 header("Location: login.php");
 exit();
